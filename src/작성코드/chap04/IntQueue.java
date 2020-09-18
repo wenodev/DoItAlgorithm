5d9964a4d8285e7de580c9d66eff1ef7c0add20c
wenodev <logicalstark1105@gmail.com>
2020-09-19 00:43:47 +0900

    04-1 스택 완료

---
+++ src/작성코드/chap04/IntQueue.java
@@ -0,0 +1,14 @@
+package 작성코드.chap04;
+
+public class IntQueue {
+
+    private int max;
+    private int front;
+    private int rear;
+    private int num;
+    private int[] que;
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/작성코드/chap04/IntStack.java
+++ src/작성코드/chap04/IntStack.java
@@ -1,5 +1,7 @@
 package 작성코드.chap04;
 
+import java.util.Scanner;
+
 public class IntStack {
 
     private int max; // 스택 용량
@@ -86,5 +88,50 @@ public class IntStack {
 
     public static void main(String[] args) {
 
+        Scanner stdIn = new Scanner(System.in);
+        IntStack s = new IntStack(64);
+
+        while(true){
+            System.out.println("현재 데이터 수 : " + s.size() + " / " + s.capacity());
+            System.out.print("(1)푸시  (2)팝  (3)피크  (4)덤프  (0)종료 : ");
+
+            int menu = stdIn.nextInt();
+
+            int x;
+            switch (menu){
+                case 1:
+                    System.out.print("데이터 : ");
+                    x = stdIn.nextInt();
+                    try {
+                        s.push(x);
+                    }catch (IntStack.OverflowIntStackException e){
+                        System.out.println("스택이 가득 찾습니다.");
+                    }
+                    break;
+
+                case 2:
+                    try {
+                        x = s.pop();
+                        System.out.println("팝한 데이터는 " + x + " 입니다.");
+                    }catch (IntStack.EmptyIntStackException e){
+                        System.out.println("스택이 비어 있습니다." + e);
+                    }
+                    break;
+                case 3:
+                    try{
+                        x = s.peek();
+                        System.out.println("피크한 데이터는 " + x + " 입니다.");
+                    }catch (IntStack.EmptyIntStackException e){
+                        System.out.println("스택이 비었씁니다." + e);
+                    }
+                    break;
+                case 4:
+                    s.dump();
+                    break;
+                default:
+                    break;
+            }
+            if(menu == 0) break;
+        }
     }
 }
