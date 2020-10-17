a132b40dba04ab1d633689c6281f44055438a38c
wenodev <logicalstark1105@gmail.com>
2020-10-17 23:47:07 +0900

    add : 하노이 문제 완료

--- src/작성코드/chap05/Hanoi_05_06.java
+++ src/작성코드/chap05/Hanoi_05_06.java
@@ -1,15 +1,28 @@
 package 작성코드.chap05;
 
+import java.util.Scanner;
+
 public class Hanoi_05_06 {
 
 
     static void move(int no, int x, int y){
+        if (no > 1)
+            move(no-1, x, 6-x-y);
+        System.out.println("원반[" + no + "]을 " + x + "기둥에서" + y + "기둥으로 옮김");
 
+        if (no > 1)
+            move(no=1,6-x-y, y);
     }
 
-
-//test
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("하노이탑");
+        System.out.println("원반 개수 : ");
+        int n = sc.nextInt();
+
+        move(n, 1, 3);
+
 
     }
 
